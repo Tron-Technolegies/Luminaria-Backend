04592835d45d222064ffa74675c8e50f0cfc507c
clawlynx <clawlynx77@gmail.com>
2026-03-19 01:17:25 +0530

    added puppeteer

--- services/pdfGeneration.js
+++ services/pdfGeneration.js
@@ -103,7 +103,11 @@ import puppeteer from "puppeteer";
 export async function generateCouponPDF(user, tagNumber) {
   const browser = await puppeteer.launch({
     headless: "new",
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    args: [
+      "--no-sandbox", 
+      "--disable-setuid-sandbox", 
+      "--disable-dev-shm-usage"
+    ],
   });
 
   const page = await browser.newPage();
